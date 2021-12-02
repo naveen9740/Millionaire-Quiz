63b3cb045cdb50b28f626e31e5c847aad93831a4
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-12-02 19:23:57 +0530

    feat: Adding Sounds

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 import { useQuizContext } from "../quizContext";
+import play from "../sounds/play.wav";
+import correct from "../sounds/correct.wav";
+import wrong from "../sounds/wrong.wav";
+
+import useSound from "use-sound";
 
 export const Trivia = () => {
   let { data, setStop, questionNumber, setQuestionNumber } = useQuizContext();
@@ -7,10 +12,27 @@ export const Trivia = () => {
   const [question, setQuestion] = useState(null);
   const [selectedAns, setSelectedAns] = useState(null);
   const [ClassName, setClassName] = useState("answer");
+  const [letsPlay] = useSound(play);
+  console.log(letsPlay);
+  const [correctAns] = useSound(correct);
+  const [wrongAns] = useSound(wrong);
+
+  useEffect(() => {
+    console.log("sss");
+    letsPlay();
+    console.log("ee");
+  }, [letsPlay]);
 
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
   }, [data, questionNumber]);
+
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+
   return (
     <div className="trivia">
       <div className="question">{question?.question}</div>
@@ -22,19 +44,26 @@ export const Trivia = () => {
               onClick={() => {
                 setSelectedAns(ans);
                 setClassName("answer active");
-                setTimeout(() => {
-                  console.log("eee");
+
+                delay(3000, () => {
                   setClassName(ans.correct ? "answer correct" : "answer wrong");
-                }, 1000);
-                setTimeout(() => {
+                });
+                delay(5000, () => {
                   if (ans.correct) {
-                    setQuestionNumber((prev) => prev + 1);
-                    setSelectedAns(null);
+                    correctAns();
+
+                    delay(1000, () => {
+                      setQuestionNumber((prev) => prev + 1);
+                      setSelectedAns(null);
+                    });
                   } else {
-                    console.log("wrrr");
+                    wrongAns();
+                    delay(1000, () => {
+                      setStop(true);
+                    });
                     setStop(true);
                   }
-                }, 3500);
+                });
               }}
             >
               {ans.text}
