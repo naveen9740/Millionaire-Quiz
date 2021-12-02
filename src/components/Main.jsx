56a1e14c58bd13ec53c999cb41038df0d5ed11bd
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-12-02 16:29:43 +0530

    feat: Trivia Component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,5 @@
+import { Trivia } from "./Trivia";
+
 export const Main = () => {
   return (
     <div className="main">
@@ -5,7 +7,9 @@ export const Main = () => {
         <div className="timer">30</div>
       </div>
 
-      <div className="bottom">question and answers</div>
+      <div className="bottom">
+        <Trivia />
+      </div>
     </div>
   );
 };
---
+++ src/components/Trivia.jsx
@@ -0,0 +1,13 @@
+export const Trivia = () => {
+  return (
+    <div className="trivia">
+      <div className="question">what ...?</div>
+      <div className="answers">
+        <div className="answer active">aaa</div>
+        <div className="answer">aaa</div>
+        <div className="answer">aaa</div>
+        <div className="answer">aaa</div>
+      </div>
+    </div>
+  );
+};
